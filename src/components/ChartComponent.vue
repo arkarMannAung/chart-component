adffb827d4288a80e7e499a30b5fb1928b0ad48f
arkarMannAung <mr.arkarmannaung@gmail.com>
2022-09-03 11:11:07 +0630

    first

---
+++ src/components/ChartComponent.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+// import * as am5 from '@amcharts/amcharts5';
+// import * as am5xy from '@amcharts/amcharts5/xy';
+// import am5themes_Animated from '@amcharts/amcharts5/themes/Animated';
+
+// interface Props {
+//     users: Array<Users>;
+// }
+interface Users {
+    name: string;
+    age: Number;
+}
+// const props = defineProps<Props>();
+const props = defineProps({
+    users: {
+        type: Array,
+        required: true,
+    }
+})
+onMounted( ()=>{
+    
+});
+</script>
+<template>
+    <div class="grid grid-cols-4 gap-8">
+        <div v-for="user in users" :key="user.id" class="">
+            <div class="rounded-lg p-8 flex-col bg-pink-300 w-[300px]">
+                <div class="my-2">
+                    name: {{user.name}}
+                </div>
+                <div class="my-2">
+                    age: {{user.age}}
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+// @ts-ignore
+import ChartComponent from '@/components/ChartComponent.vue';
+import { defineComponent } from '@vue/runtime-core';
+const users = [
+  {name: 'Jhon Doe a',age: 'string'},
+  {name: 'Jhon Doe b',age: 25},
+  {name: 'Jhon Doe b',age: 30},
+  {name: 'Jhon Doe c',age: 20},
+];
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <ChartComponent :users="users" />
   </div>
 </template>
