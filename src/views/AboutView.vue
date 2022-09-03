97517fde2b288b906b6bf84160f93f2fa9afeb48
mztcandle97 <may@kitesense.sg>
2022-09-03 15:49:45 +0800

    update amchart

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,18 +1,53 @@
-<script setup lang="ts">
-// @ts-ignore
-import ChartComponent from '@/components/ChartComponent.vue';
+<script lang="ts">
+import ColumnChart from '../components/amchart/ColumnChart.vue';
 import { defineComponent } from '@vue/runtime-core';
-const users = [
-  {name: 'Jhon Doe a',age: 'string'},
-  {name: 'Jhon Doe b',age: 25},
-  {name: 'Jhon Doe b',age: 30},
-  {name: 'Jhon Doe c',age: 20},
-];
+import { computed } from "vue";
+
+
+// const users = [
+//   {name: 'Jhon Doe a',age: 'string'},
+//   {name: 'Jhon Doe b',age: 25},
+//   {name: 'Jhon Doe b',age: 30},
+//   {name: 'Jhon Doe c',age: 20},
+// ];
+
+export default defineComponent({
+
+components: {
+  ColumnChart,
+
+},
+ setup() {
+ 
+    const chartSeries = computed(() => [
+    {
+      name: "Question(s)",
+      data:  [3, 2,4, 1],
+    },
+  ]);
+ 
+
+return {
+    chartSeries
+  }
+ },
+
+});
 </script>
 
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <ChartComponent :users="users" />
+  <div class="tw-mt-8" style="width: 80%" >
+    <h1>XY Column Chart</h1>
+    <ColumnChart  />
   </div>
 </template>
+
+<!-- <template>
+  <div id="chart" >
+    <CohortContentViews height="400" :series="series" :options="chartOptions" />
+  </div>
+</template> -->
+
+
+
